79bceb53b5d60c6ec05aaac3d1af6cda48f6b9a0
是简瞳啊 <jiantong4869@163.com>
2021-08-25 14:19:43 +0800

    优化

--- src/network/config.js
+++ src/network/config.js
@@ -2,7 +2,7 @@ let base_url = ''
 if (process.env.NODE_ENV === 'development') {
   base_url = 'http://localhost:3000'
 } else if (process.env.NODE_ENV === 'production') {
-  base_url = 'http://110.42.138.85/'
+  base_url = 'https://jiantong.vercel.app/'
 }
 export default {
   base_url
