7ee0c2fdd9c00a870e37db24aa0d5e935ca2faf1
是简瞳啊 <jiantong4869@163.com>
2021-08-25 10:10:43 +0800

    项目优化

--- src/network/config.js
+++ src/network/config.js
@@ -2,7 +2,7 @@ let base_url = ''
 if (process.env.NODE_ENV === 'development') {
   base_url = 'http://localhost:3000'
 } else if (process.env.NODE_ENV === 'production') {
-  base_url = ''
+  base_url = 'http://110.42.138.85/'
 }
 export default {
   base_url
